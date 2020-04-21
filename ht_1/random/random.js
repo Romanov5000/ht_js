19a84c68cee4168b74945751ab673e11b45f9946
Romanov5000 <romanovvladislav97@gmail.com>
2020-04-21 13:11:15 +0300

    'fix_random'

--- ht_1/random/random.js
+++ ht_1/random/random.js
@@ -11,17 +11,11 @@ for ( let i = 10 ; i > numberRandom.length ; numberRandom.push(createRandom()) )
 }
 
     for ( let i = 0 ; i < numberRandom.length ; i++ ){
-        if (numberRandom [i] % 10 === 0 ) {
-            numberRandom [i] = String (numberRandom [i]) [0] + 'zero' ;      
-        } else if (numberRandom [i] === 0 ){
-            numberRandom [i] = 'zero' ;
+        if (numberRandom [i] === 0 ){
+            numberRandom [i] = 'zero' ;      
+        } else if (numberRandom [i] % 10 === 0 ) {
+            numberRandom [i] = String (numberRandom [i]) [0] + 'zero' ;
         }
 }
 
 console.log ( numberRandom );
-
-
-
-
-
-
