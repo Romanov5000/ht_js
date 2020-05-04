6e3b14509a8e6d5727ff761b25a57896207cb245
Romanov5000 <romanovvladislav97@gmail.com>
2020-05-04 22:25:26 +0300

    'table'

---
+++ ht_3/js/script.js
@@ -0,0 +1,86 @@
+const data = [
+    {
+        firstName: 'Ashton',
+        lastName: 'Kutcher',
+        age: 40
+    }, {
+        firstName: 'Bradley',
+        lastName: 'Pitt',
+        age: 54
+    }, {
+        firstName: 'Hannah',
+        lastName: 'Dakota',
+        age: 24
+    }
+];
+const addUserBtn = document.querySelector('.add_user_btn');
+const tbody = document.querySelector('table tbody');
+const formAddUser = document.querySelector('form');
+
+
+function deleteUser() {
+    let deleteTr = this.closest('tr');
+    deleteTr.remove();
+}
+function addTdWithBtns(tr) {
+    let deleteUserBtn = document.createElement('button');
+    let editUserBtn = document.createElement('button');
+    let btnsTd = document.createElement('td');
+
+    deleteUserBtn.className = 'delete_user_btn';
+    editUserBtn.className = 'edit_user_btn';
+    btnsTd.className = 'btns_td';
+
+    deleteUserBtn.addEventListener ( 'click', deleteUser );
+    editUserBtn.addEventListener ('click' , changeUserName );
+
+    deleteUserBtn.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+    editUserBtn.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
+
+    btnsTd.append(editUserBtn);
+    btnsTd.append(deleteUserBtn);
+    tr.append(btnsTd);
+}
+
+for(let i = 0; i < data.length; i++) {
+    let tr = document.createElement('tr');
+
+    for(let key in data[i]) {
+        let td = document.createElement('td');
+        td.innerHTML = data[i][key];
+        tr.append(td);
+    }
+
+    addTdWithBtns(tr);
+    
+    tbody.append(tr);
+}
+
+formAddUser.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let inputNameValue = event.target.querySelector('.input_name').value;
+    let inputSecondNameValue = event.target.querySelector('.input_second_name').value;
+    let inputAgeValue = event.target.querySelector('.input_age').value;
+    
+    let tr = document.createElement('tr');
+    tr.innerHTML = `            
+    <td>${inputNameValue}</td>
+    <td>${inputSecondNameValue}</td>
+    <td>${inputAgeValue}</td>`;
+    addTdWithBtns(tr);
+    tbody.append(tr);
+    
+});
+
+function changeUserName () {
+ 
+     changeUerFirstName = prompt ('Введите новое имя пользователя');
+     changeUerLastName = prompt ('Введите новую фамилию пользователя');
+     changeUerAge = prompt ('Введите новый возраст пользователя');
+ 
+     let tr = this.closest('tr');
+     tr.innerHTML = `<td>${changeUerFirstName}</td><td>${changeUerLastName}</td><td>${+changeUerAge}</td>`;
+
+     addTdWithBtns(tr);
+     
+}
